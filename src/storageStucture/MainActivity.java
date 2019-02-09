8a2823f9cdb16e37e6a26b9c822b4485fed0626f
Unknown <sam@malpass.me.uk>
2019-02-09 16:32:50 +0000

    Add variables

--- src/storageStucture/MainActivity.java
+++ src/storageStucture/MainActivity.java
@@ -4,9 +4,16 @@
  */
 package storageStucture;
 
+import java.util.ArrayList;
+
 public class MainActivity extends Activity {
     /**
      * serialVersionUID allows the class to implement serialised
      */
     public static final long serialVersionUID = 1L;
+    /**
+     * subOptions holds a list of sub-options if they are needed
+     */
+    ArrayList<String> subOptions;
+
 }
