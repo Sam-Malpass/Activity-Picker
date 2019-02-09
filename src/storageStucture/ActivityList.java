522e5ddbc00bf397074e73b80377a7fe3717a86e
Unknown <sam@malpass.me.uk>
2019-02-09 16:46:21 +0000

    Add variables

--- src/storageStucture/ActivityList.java
+++ src/storageStucture/ActivityList.java
@@ -4,9 +4,22 @@
  */
 package storageStucture;
 import java.io.Serializable;
+import java.util.ArrayList;
 public class ActivityList implements Serializable {
     /**
      * serialVersionUID allows the class to implement serialised
      */
     public static final long serialVersionUID = 1L;
+    /**
+     * listID holds the ID of the list
+     */
+    private long listID;
+    /**
+     * listName holds the name of the list
+     */
+    private String listName;
+    /**
+     * activities holds a list of Activity objects
+     */
+    private ArrayList<Activity> activities;
 }
