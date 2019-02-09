79d45a298113576bb05bb39bf94b0146e6b6f496
Unknown <sam@malpass.me.uk>
2019-02-09 16:42:38 +0000

    Create class

---
+++ src/storageStucture/ActivityList.java
@@ -0,0 +1,12 @@
+/**
+ * ActivityList
+ * @author Team Jam
+ */
+package storageStucture;
+import java.io.Serializable;
+public class ActivityList implements Serializable {
+    /**
+     * serialVersionUID allows the class to implement serialised
+     */
+    public static final long serialVersionUID = 1L;
+}
