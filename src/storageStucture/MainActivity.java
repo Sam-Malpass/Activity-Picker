42c2fb915b1965ac24deb30ca9ea305f0aff8ac9
Unknown <sam@malpass.me.uk>
2019-02-09 16:37:20 +0000

    Add constructors

--- src/storageStucture/MainActivity.java
+++ src/storageStucture/MainActivity.java
@@ -3,9 +3,7 @@
  * @author Team Jam
  */
 package storageStucture;
-
 import java.util.ArrayList;
-
 public class MainActivity extends Activity {
     /**
      * serialVersionUID allows the class to implement serialised
@@ -15,5 +13,28 @@ public class MainActivity extends Activity {
      * subOptions holds a list of sub-options if they are needed
      */
     ArrayList<String> subOptions;
-
+    /**
+     * Constructor with no arguments
+     * <p>
+     *     Sets up the object
+     * </p>
+     */
+    public MainActivity() {
+        super();
+        subOptions = null;
+    }
+    /**
+     * Constructor with arguments
+     * <p>
+     *     Sets up the object using the passed values
+     * </p>
+     * @param ID is the activityID
+     * @param name is the name for the activity
+     * @param desc is the description for the activity
+     * @param subs is the sub-list of activities
+     */
+    public MainActivity(long ID, String name, String desc, ArrayList<String> subs) {
+        super(ID, name, desc);
+        subOptions = subs;
+    }
 }
